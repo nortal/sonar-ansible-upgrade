c92ffb1d08c497962eb3a4b025159b8057b73f0b
priitliivak <Priit.Liivak@nortal.com>
2016-03-29 21:18:49 +0300

    initial

---
+++ playbooks/roles/nortal-sonar-config/templates/sonar.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+SONAR_HOME={{ sonar_home }}
+/bin/bash $SONAR_HOME/bin/linux-x86-64/sonar.sh $@
---
+++ tests/run_docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+TESTS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+TEST_COMMAND="cd /playbooks && ansible-playbook -i 'localhost,' -c local --extra-vars 'sonar_db_create_backup=false' upgrade-sonar.yml"
+
+# Cleanup leftover containers
+docker rm $(docker ps -a -q)
+
+# Run container mounting playbooks as volume
+docker run -v "$TESTS_DIR/../playbooks/:/playbooks:rw" 'sonar-upgrade-test' /bin/bash -c "${TEST_COMMAND}"
